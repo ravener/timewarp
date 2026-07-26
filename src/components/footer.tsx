bae3123bb95a019fadba32e85aac6036fd0617e3
Raven Ravener <ravener.anime@gmail.com>
2026-07-26 22:50:52 +0500

    fix github url

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     <footer className="pt-2.5 pb-1 text-[10px] bg-[#2a2226]">
       <div className="mb-1 flex justify-center flex-wrap">
         <a className="font-bold px-2.5" href="https://discord.gg/DQ2BgNDb6Q" target="_blank">Discord</a>
-        <a className="font-bold px-2.5" href="https://github.com/ravener" target="_blank">Source Code</a>
+        <a className="font-bold px-2.5" href="https://github.com/ravener/timewarp" target="_blank">Source Code</a>
         <a className="font-bold px-2.5" href="https://ravener.is-a.dev/donate" target="_blank">Donate</a>
       </div>
       <div className="mb-1 flex justify-center flex-wrap text-[#74646c]"><span>Made by <a href="https://github.com/ravener" target="_blank">Ravener</a> (not affiliated with ppy or osu!)</span></div>
